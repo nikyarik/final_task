3fa3786733ee565093fcdb2cd655345d525828fb
Yaroslav Husiev <bingo@dyit.org.ya>
2022-08-23 21:58:05 +0200

    ch amiiii

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,14 +19,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c9354388bb36c088"
   instance_type = "t2.micro"
   key_name      = "aws-rsa-key-august-2022"
-  user_data     = << EOF
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-EOF
+  user_data = << EOF
+	#! /bin/bash
+        sudo apt-get update
+	sudo apt-get install -y apache2
+	sudo systemctl start apache2
+	sudo systemctl enable apache2
+	echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+  EOF
   tags = {
     Name = var.instance_name
   }
