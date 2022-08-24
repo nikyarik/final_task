5b8e1401aeee094e65f8bfb0b3e34b28d524d6de
Yaroslav Husiev <bingo@dyit.org.ya>
2022-08-24 23:28:09 +0200

    edit user_data

--- infrastructure/install_apache.sh
+++ infrastructure/install_apache.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+#sudo apt-get install -y apache2
+#sudo systemctl start apache2
+#sudo systemctl enable apache2
+#echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 sudo apt-get remove docker docker.io containerd runc
 sudo apt install -y  apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
