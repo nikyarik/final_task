c2e087fd38cb6777c1cfbc51842866c3fb5cc480
Yaroslav Husiev <bingo@dyit.org.ya>
2022-08-24 20:32:33 +0200

    add info

--- infrastructure/install_apache.sh
+++ infrastructure/install_apache.sh
@@ -4,3 +4,13 @@ sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+sudo apt-get remove docker docker.io containerd runc
+sudo apt install -y  apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt update
+sudo apt -y install docker-ce
+sudo adduser ubuntu docker
+sudo docker run -d -p 8080:8888 --name itea_go bing0id/itea_go:latest
+
